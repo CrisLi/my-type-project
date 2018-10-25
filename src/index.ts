052a6eaa41061821de40cfc838a2cbb726e535c3
燃柏 <ranbai.lb@alibaba-inc.com>
2018-10-25 11:19:31 +0800

    Add count down function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export const countDown = (from: number) => {
+  return interval(1000)
+    .pipe(
+      map(value => (from - value)),
+      take(from)
+    );
+};
