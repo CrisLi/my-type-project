8b5b32db19029d62472214a5553bbf28476d0e2e
燃柏 <ranbai.lb@alibaba-inc.com>
2018-10-25 11:19:38 +0800

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import { map, take } from 'rxjs/operators';
 export const Greeter = (name: string) => `Hello ${name}`;
 
 export const countDown = (from: number) => {
-  return interval(1000)
-    .pipe(
-      map(value => (from - value)),
-      take(from)
-    );
+  return interval(1000).pipe(
+    map(value => from - value),
+    take(from),
+  );
 };
