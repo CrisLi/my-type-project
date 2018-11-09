16f9003e7237652d000fde703e5e5a004f7e52cb
燃柏 <ranbai.lb@alibaba-inc.com>
2018-11-09 13:34:54 +0800

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export const hello = (name: string): string => `Hello ${name}`;
 export const countDown = (from: number): Observable<number> => {
   return timer(0, 1000).pipe(
     map(value => from - value),
-    take(from + 1)
+    take(from + 1),
   );
 };
